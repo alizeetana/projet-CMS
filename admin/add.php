d9587bd3766e669479191e3464d78a46b7154b81
Alizee.t <alizetana@gmail.com>
2019-05-21 21:22:01 +0200

    ajout du delete

--- admin/add.php
+++ admin/add.php
@@ -9,23 +9,22 @@ if(isset($_SESSION['logged_in'])) {
 
 	if (isset($_POST['title'], $_POST['content'])) {
 			$title = $_POST['title'];
-			$content = $_POST['content'];
+			$content = nl2br($_POST['content']);
+
 
 			if (empty($title) or empty($content)) {
 
 				$error = 'All fields are required!';
 			} else {
 
-				$query = $pdo->prepare('ISERT INTO articles (article_title, article_content, article_timestamp) VALUES (?, ?, ?,)');
+				$query = $pdo->prepare('INSERT INTO articles (article_title, article_content, article_timestamp) VALUES (?, ?, ?)');
 
 				$query->bindValue(1, $title);
 				$query->bindValue(2, $content);
 				$query->bindValue(3, time());
 
 				$query->execute();
-				/*?>
-				<?php echo $query; ?>
-				<?php */
+				
 				header('Location: index.php');
 			}
 
---
+++ admin/delete.php
@@ -0,0 +1,48 @@
+<?php
+
+session_start();
+
+include_once('../includes/connection.php');
+include_once('../includes/article.php');
+
+$article = new Article; 
+
+if (isset($_SESSION['logged_in'])) { 
+	$articles = $article->fetch_all();
+	?>
+	<html>
+			<head>
+				<title>CMS Platform</title>
+				<link rel="stylesheet" href="../assets/style.css" />
+			</head>
+
+			<body>
+				<div class="container">
+					<a href="index.php" id="logo">CMS</a>	
+
+					<br />
+
+					<form action="delete.php" method="get">
+						<select onchange="this.form.submit();">
+							
+							<?php foreach ($artcles as $article) { ?>
+
+								<option value="<?php echo $article['article_id']; ?>"><?php echo $article['article_title;'] ?></option>
+
+							<?php } ?>
+
+						</select>
+					</form>	
+				</div>
+			</body>
+		</html>
+
+<?php
+} else {
+
+	header('Location: index.php')
+
+
+}
+
+?>
